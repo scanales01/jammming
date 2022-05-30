ed7e2f934f0e0f9817711ff8c0faaccd8d093841
scanales01 <scanal01@outlook.com>
2022-05-29 22:28:17 -0500

    Playlist Name Change Functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName =this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -85,7 +86,8 @@ class App extends React.Component {
              <Playlist
                 playlistName={this.state.playlistName}
                 playlistTracks={this.state.playlistTracks}
-                onRemove={this.removeTrack}/>
+                onRemove={this.removeTrack}
+                onNameChange={this.updatePlaylistName}/>
            </div>
         </div>
       </div>
